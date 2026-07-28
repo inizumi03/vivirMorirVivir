0d7bc7b8ec33c0f8f5f80ebd3daa53656012cc53
inizumi03 <147955660+inizumi03@users.noreply.github.com>
2026-07-27 21:16:32 -0300

    se empezo el nivel 0

---
+++ morir Vivir morir/Assets/1 Codigos/jugador/robot base/RobotBase.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotBase : MonoBehaviour
+{
+    [Header("Estadísticas")]
+    [Min(0)]
+    public int peso = 1;
+}
